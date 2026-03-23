9f149c468f03c8cbce115ebec515226d64f672e7
abdelouahedmariam <abdelouahedmariam@gmail.com>
2026-03-23 20:49:07 +0100

    refactor task routes into routes folder

--- src/app.js
+++ src/app.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const tasksRouter = require('./routes/tasks'); 
 
 let tasks = [
-  { id: 1, title: "Initial task", completed: true },
+  { id: 1, title: "Initial task lab 1", completed: true },
   { id: 2, title: "Install Git and Node.js", "completed": true },
   { id: 2, title: "Learn DevOps basics", completed: false }
   
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "Welcome from MAIN branch (lab1)" });
+  res.json({ message: "Welcome from MAIN branch (lab2)" });
 });
 
 app.get('/tasks', (req, res) => {
   res.json(tasks);
 });
-
 app.post('/tasks', (req, res) => {
   const newTask = { id: tasks.length+1, title: req.body.title, completed: false };
   tasks.push(newTask);
   res.status(201).json(newTask);
 });
+app.use('/tasks', tasksRouter); 
 
 app.listen(3000, ()=> console.log("API running on port 3000"));
